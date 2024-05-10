800cf7495055499242c4bc2d05fd81a389364960
beckerme <ewertonbecker12@gmail.com>
2024-05-10 18:48:55 +0100

    :dizzy: animacoes: adiciona animacoes ao css juntamente com o js

--- assets/js/javascript-menu.js
+++ assets/js/javascript-menu.js
@@ -3,6 +3,18 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu() {
     const nav = document.getElementById('nav-b');
     nav.classList.toggle('active'); 
+    btnMobile.classList.toggle('active');
+
+    const btnIcon = document.querySelector('.bi');
+    if (nav.classList.contains('active')) {
+        btnIcon.classList.remove('bi-list'); // Remove a classe do ícone de lista
+        btnIcon.classList.add('bi-x-lg'); // Adiciona a classe do ícone "x"
+        btnMobile.style.animation = 'spin 0.3s forwards';
+    } else {
+        btnIcon.classList.remove('bi-x-lg'); // Remove a classe do ícone "x"
+        btnIcon.classList.add('bi-list'); // Adiciona a classe do ícone de lista
+        btnMobile.style.animation = 'spin-reverse 0.3s forwards'; // Remove a animação
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
